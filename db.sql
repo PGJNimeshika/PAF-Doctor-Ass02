fbd9b58e5b6a645ef7f500a9c9d8444a07bbd415
PGJNimeshika <j.nimeshika.sandu@gmail.com>
2020-05-06 09:08:25 +0530

    add more details to the data base(valid)

--- db.sql
+++ db.sql
@@ -61,29 +61,39 @@ CREATE TABLE prescription (
 );
 
 INSERT INTO doctor (doctor_license_no, doctor_name, doctor_address, doctor_phone, doctor_gender, doctor_email, doctor_specialization)
-VALUES ('GMOA1', 'Nimeshika', 'Colombo', '0777123456', 'female', 'test@test.com', 'sepc1'),
-       ('GMOA2', 'Jehani', 'Piliyandala', '0711234569', 'female', 'one@test.com', 'sepc2');
+VALUES ('GMOA1', 'Nimeshika', 'Colombo', '0777123456', 'female', 'test@test.com', 'eye specialist'),
+       ('GMOA2', 'Jehani', 'Piliyandala', '0711234569', 'female', 'one@test.com', 'Cardiologist'),
+       ('GMOA2', 'Pavithra', 'Piliyandala', '0711234569', 'female', 'one@test.com', 'Physician'),
+       ('GMOA2', 'Purnima', 'Piliyandala', '0711234569', 'female', 'one@test.com', 'Surgeon');
 
 INSERT INTO doctor_logins(doctor_id, doctor_username, doctor_password, doctor_last_login, docotor_session) 
-VALUES ('1', 'nim', sha1('abc123'), NOW(), ''),
-       ('2', 'jehani', sha1('abc123'), NOW(), '');
+VALUES ('1', 'nimeshika', sha1('abc123'), NOW(), ''),
+       ('2', 'jehani', sha1('abc123'), NOW(), ''),
+       ('3', 'pavithra', sha1('abc123'), NOW(), ''),
+       ('4', 'purnima', sha1('abc123'), NOW(), '');
 
 
 INSERT INTO doctor_schedule (schedule_date, doctor_id, time_slot)
 VALUES (CURRENT_DATE(), '1', '1600-1700'),
-       (CURRENT_DATE(), '1', '2000-2200'),
-       (CURRENT_DATE(), '2', '1000-1200');
+       (CURRENT_DATE(), '2', '2000-2200'),
+       (CURRENT_DATE(), '3', '1000-1200'),
+       (CURRENT_DATE(), '4', '1400-1200');
 
 
 INSERT INTO patient (patient_name, patient_age)
 VALUES ('Isira', 28),
-       ('Kasun', 28);
+       ('Kasun', 25),
+       ('pooja', 21),
+       ('pini', 16);
 
 INSERT INTO appointment (patient_id, appoint_date, appoint_time, appoint_status, doctor_id)
-VALUES ('1', CURRENT_DATE(), CURTIME(), 'pending', '1'),
-       ('2', CURRENT_DATE(), '20:00:00', 'pending', '2'),
-       ('2', CURRENT_DATE(), '10:00:00', 'pending', '2');
+VALUES ('1', CURRENT_DATE(), CURTIME(), 'pending','1'),
+       ('2', CURRENT_DATE(), '20:00:00', 'accept','2'),
+       ('3', CURRENT_DATE(), '10:00:00', 'pending','3'),
+       ('4', CURRENT_DATE(), '10:00:00', 'accept', '4');
 
 INSERT INTO prescription (patient_id, pres_date, pres_time, descript, doctor_id, appoint_id)
-VALUES  ('1', CURRENT_DATE(), CURTIME(), 'description 1', '1', '1'),
-        ('2', CURRENT_DATE(), CURTIME(), 'description 2', '2', '2');
+VALUES  ('1', CURRENT_DATE(), CURTIME(), 'need to check again', '1', '1'),
+        ('2', CURRENT_DATE(), CURTIME(), 'give prescription', '2', '2'),
+        ('3', CURRENT_DATE(), CURTIME(), 'prescriptions for two weeks', '3', '3'),
+        ('4', CURRENT_DATE(), CURTIME(), 'need to check again', '4', '4');
